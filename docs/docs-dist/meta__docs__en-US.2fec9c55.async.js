2ada7b4e9eeb27473d6d36785b88432a474dc5ef
allenyan <yanlezhong@gmail.com>
2026-02-24 23:31:48 +0800

    feat: Upgrade Dumi to v2 and update core dependencies, including adding lodash and its types.

--- docs/docs-dist/meta__docs__en-US.2fec9c55.async.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_fiagram_docs=self.webpackChunk_fiagram_docs||[]).push([[430],{43654:function(t,u,n){n.r(u),n.d(u,{demos:function(){return d}});var s=n(92379),d={}},98831:function(t,u,n){n.r(u),n.d(u,{texts:function(){return s}});const s=[]}}]);
